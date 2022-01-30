ce17089a96c4fecca7a114af4d13a5cf4bd8b026
asy-init <94440862+asy-init@users.noreply.github.com>
2022-01-30 12:38:47 +0530

    Small change

---
+++ template/settings.py
@@ -0,0 +1,13 @@
+from discord import Intents
+
+# place token here
+TOKEN = "token123"
+
+# prefix ( mention will also trigger dw jus put prefix)
+PREFIX = "d?"
+
+# 5 status slots that keep changing
+STATUS = [" ", " ", " ", " ", " "]
+
+# you can change intents to what ever you .default() or .all() or anything in between
+INTENT = Intents.default()
